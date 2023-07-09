944e30dc0d331378a184c937d383dec3bba7dc46
Daniel Tovar <91617620+Danny112T@users.noreply.github.com>
2023-07-08 23:39:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student","Developer", "Cartoonist", "Designer", "Freelancer","iDanny"],
+        strings: ["Student","Developer", "Cartoonist", "Designer", "Freelancer","Un wey cualquiera","iDanny"],
         typeSpeed: 100,
         backSpeed: 60,
         cursorChar: '|',
